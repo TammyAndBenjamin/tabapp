c09dae3e6445f11bcba08e388a33ee881d4ce8c1
Jonathan Camile <jonathan@meilleursagents.com>
2014-04-16 08:02:11 +0000

    Update reporting screen

--- application.py
+++ application.py
@@ -59,9 +59,71 @@ def aggregate_orders(orders):
         aggregate[customer_email] = row
     return aggregate
 
+def process_orders(orders):
+    rows = []
+    for order in orders:
+        customer = order.get('customer')
+        lines = order.get('line_items')
+        tax_lines = order.get('tax_lines')
+        discount_value = float(order.get('total_discounts'))
+        row = {
+            'order_no': order.get('name'),
+            'customer_firstname': customer.get('first_name'),
+            'customer_lastname': customer.get('last_name'),
+            'customer_email': customer.get('email'),
+            'products': [],
+            'excluding_taxes_amount': 0,
+            'discount_amount': 0,
+            'cost_amount': 0,
+            'benefits': 0,
+        }
+        for line in lines:
+            taxes = line.get('tax_lines')
+            row['products'].append(line.get('title'))
+            price = float(line.get('price'))
+            if order.get('taxes_included'):
+                for tax_line in taxes:
+                    tax_rate = float(tax_line.get('rate'))
+                    row['excluding_taxes_amount'] += price / (1 + tax_rate)
+            else:
+                row['excluding_taxes_amount'] += price
+        if order.get('taxes_included'):
+            for tax_line in tax_lines:
+                tax_rate = float(tax_line.get('rate'))
+                row['discount_amount'] += discount_value / (1 + tax_rate)
+        else:
+            row['discount_amount'] += discount_value
+        row['benefits'] = row['excluding_taxes_amount'] - row['discount_amount'] - row['cost_amount']
+        rows.append(row)
+    return rows
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/analytics')
+def analytics():
+    date_lbound = request.args.get('date_lbound')
+    date_ubound = request.args.get('date_ubound')
+    fields = [
+        'name',
+        'created_at',
+        'customer',
+        'tags',
+        'line_items',
+        'taxes_included',
+        'tax_lines',
+        'total_discounts',
+    ]
+    url = '{base_url}orders.json?page={{page}}&fields={fields}{date_lbound}{date_ubound}'.format(**{
+            'base_url': base_url,
+            'fields': ','.join(fields),
+            'date_lbound': '&updated_at_min={}'.format(date_lbound) if date_lbound else '',
+            'date_ubound': '&updated_at_max={}'.format(date_ubound) if date_ubound else '',
+        })
+    orders = list_from_api(url, 'orders')
+    rows = process_orders(orders)
+    return jsonify(rows=rows)
 
 @app.route('/orders/', defaults={'page': 1})
 @app.route('/orders/<int:page>')

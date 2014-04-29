20a31740af07317676b9b24e7a412e4935790ae6
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-04-29 19:00:30 +0200

    WIP Add and Display costs history

--- product_costs.py
+++ product_costs.py
@@ -3,13 +3,25 @@
 from flask import Blueprint, request, render_template, jsonify, g
 from utils import list_from_api, process_orders
 import math
+import psycopg2.extras
 
 product_costs_bp = Blueprint('product_costs_bp', __name__)
 
+def current_cost(cur, product_id):
+    cur.execute('''
+        SELECT id, value
+        FROM product_cost
+        WHERE product_id = %s
+        AND daterange(start_date, end_date) @> current_date
+        ORDER BY end_date DESC NULLS FIRST
+    ''', (product_id,))
+    product_cost = cur.fetchone()
+    return product_cost
+
 @product_costs_bp.route('/')
 def products_list():
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     page = int(request.args.get('page', 1))
-    cur = g.db.cursor()
     fields = [
         'id',
         'title',
@@ -23,19 +35,12 @@ def products_list():
     rows = list_from_api(url, 'products', page)
     products = []
     for row in rows:
-        cur.execute('''
-            SELECT value
-            FROM product_cost
-            WHERE product_id = %s
-            AND daterange(start_date, end_date) @> current_date
-            ORDER BY end_date DESC NULLS FIRST
-        ''', (row.get('id'),))
-        product_cost = cur.fetchone()
+        product_cost = current_cost(cur, row.get('id'))
         product = {
             'id': row.get('id'),
             'title': row.get('title'),
             'price': row.get('variants')[0].get('price'),
-            'cost': product_cost[0] if product_cost else None,
+            'cost': product_cost['value'] if product_cost else None,
         }
         products.append(product)
     context = {
@@ -47,24 +52,33 @@ def products_list():
 
 @product_costs_bp.route('/<int:product_id>', methods=['GET'])
 def costs_history(product_id):
-    cur = g.db.cursor()
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute('''
         SELECT value, start_date
         FROM product_cost
         WHERE product_id = %s
-        AND daterange(start_date, end_date) @> current_date
         ORDER BY start_date ASC, end_date ASC NULLS LAST
     ''', (product_id,))
     rows = cur.fetchall()
+    print(rows)
     product_costs = []
     for row in rows:
         product_cost = {
-            'date': row[1].isoformat(),
-            'value': row[0],
+            'date': row['start_date'].isoformat(),
+            'value': row['value'],
         }
         product_costs.append(product_cost)
     return jsonify(costs=product_costs)
 
 @product_costs_bp.route('/<int:product_id>', methods=['POST'])
 def add_cost(product_id):
-    pass
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    product_cost = current_cost(cur, product_id)
+    if product_cost:
+        cur.execute('''
+            UPDATE product_cost SET end_date = current_date WHERE id = %s
+        ''', (product_cost['id'],))
+    cur.execute('''
+        INSERT INTO product_cost(product_id, value, start_date) VALUES(%s, %s, current_date)
+    ''', (product_id, request.form.get('product_cost'),))
+    return jsonify(result=True)

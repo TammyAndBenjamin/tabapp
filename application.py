53eafa4ad61ae3fd72095fa805ca984e7190ce80
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-09-23 00:36:21 +0200

    Switch to SQLAlchemy

--- application.py
+++ application.py
@@ -4,12 +4,12 @@
 from flask import Flask, render_template, g
 from flask_wtf.csrf import CsrfProtect
 from flask.ext.login import LoginManager, login_required
+from flask.ext.sqlalchemy import SQLAlchemy
 from orders import orders_bp
 from product_costs import product_costs_bp
 from retailers import retailers_bp
 from users import users_bp
 from supply import supply_bp
-from user import User
 import psycopg2
 
 
@@ -24,31 +24,42 @@ app.register_blueprint(users_bp, url_prefix='/login')
 app.register_blueprint(supply_bp, url_prefix='/supplies')
 CsrfProtect(app)
 
+db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'users_bp.login'
 
 
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, index = True, unique = True)
+    password = db.Column(db.String)
+
+    def is_authenticated(self):
+        return bool(self.id)
+
+
+    def is_active(self):
+        return bool(self.id)
+
+
+    def is_anonymous(self):
+        return not bool(self.id)
+
+
+    def get_id(self):
+        return str(self.id)
+
+
 @login_manager.user_loader
 def load_user(userid):
-    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute('''
-        SELECT *
-        FROM login
-        WHERE id = %s
-    ''', (userid,))
-    row = cur.fetchone()
-    if not row:
-        return None
-    return User(row)
+    return Login.query.get(userid)
 
 
 @app.before_request
 def init_request():
-    if not app.db:
-        app.db = psycopg2.connect(host=app.config['DB_HOST'], user=app.config['DB_USER'], dbname=app.config['DB_NAME'])
-        app.db.autocommit = True
-    g.db = app.db
+    g.db = db
     g.config = app.config
 
 
--- users.py
+++ users.py
@@ -2,7 +2,6 @@
 
 from flask import Blueprint, request, render_template, g, redirect, url_for, flash, current_app
 from flask.ext.login import login_required, logout_user, login_user
-from user import User
 import hashlib
 import psycopg2.extras
 
@@ -16,16 +15,10 @@ def login():
     cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
     username = request.form.get('username')
     salted_password = g.config['SECRET_KEY'] + request.form.get('password')
-    cur.execute('''
-        SELECT *
-        FROM login
-        WHERE username = %s
-        AND password = %s
-    ''', (username, hashlib.md5(salted_password.encode('ascii')).hexdigest()))
-    row = cur.fetchone()
-    if not row:
+    hashed_password = hashlib.md5(salted_password.encode('ascii'))
+    user = db.Login.query.filter_by(username=username, password=hashed_password.hexdigest()).first()
+    if not user:
         return render_template('login.html')
-    user = User(row)
     login_user(user)
     return redirect(request.args.get('next') or url_for('index'))
 

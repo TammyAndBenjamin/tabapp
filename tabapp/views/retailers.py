15fd5e46f701c0ead9b8fd412553665e865d5e3e
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-09-27 15:19:08 +0200

    Rewrite retailers view

--- tabapp/views/retailers.py
+++ tabapp/views/retailers.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from datetime import date
 from flask import Blueprint, request, render_template, g, redirect, url_for, flash, current_app
 from flask.ext.login import login_required
+from tabapp import db
+from tabapp.models import Retailer, RetailerProduct
 import tabapp.utils
 import math
-import psycopg2.extras
+import sqlalchemy
+import sqlalchemy.dialects.postgresql
 
 retailers_bp = Blueprint('retailers_bp', __name__, subdomain='backyard')
 
 
 def structure_from_rows(rows):
     for row in rows:
-        product_id = row['product_id']
-        retailer_name = row['retailer_name']
-        product_order_ids = row['product_order_ids']
+        product_id = row.product_id
+        retailer_name = row.name
+        product_order_ids = row.product_order_ids
         fields = [
             'id',
             'title',
@@ -43,16 +47,17 @@ def add_product_order(form):
         raise Exception('Please choose a retailer')
     product_ids = [ int(v) for v in form.getlist('product_id') ]
     quantities = [ int(v) for v in form.getlist('quantity') ]
-    sql = '''
-        INSERT INTO retailer_product(retailer_id, product_id, order_date)
-        VALUES (%s, %s, current_date)
-    '''
     cart = zip(product_ids, quantities)
     for product_id, quantity in cart:
         if not quantity:
             continue
         for i in range(quantity):
-            tabapp.utils.execute(cur, sql, (retailer_id, product_id))
+            retailer_product = RetailerProduct()
+            retailer_product.retailer_id = retailer_id
+            retailer_product.product_id = product_id
+            retailer_product.order_date = date.today()
+            db.session.add(retailer_product)
+    db.session.commit()
     return redirect(url_for('retailers_bp.invoices', **{'retailer_id': retailer_id}))
 
 
@@ -61,13 +66,10 @@ def sold_product_order(form):
     if not retailer_id:
         raise Exception('Please choose a retailer')
     product_order_ids = form.getlist('product_order_id')
-    sql = '''
-        UPDATE retailer_product
-        SET sold_date = current_date
-        WHERE id = %s
-    '''
     for product_order_id in product_order_ids:
-        tabapp.utils.execute(cur, sql, (product_order_id,))
+        retailer_product = RetailerProduct.query.get(product_order_id)
+        retailer_product.sold_date = date.today()
+    db.session.commit()
     return redirect(url_for('retailers_bp.invoices', **{'retailer_id': retailer_id}))
 
 
@@ -94,7 +96,7 @@ def index():
         except Exception as e:
             for msg in e.args:
                 flash(msg, 'error')
-    retailers = g.db.Retailer.all()
+    retailers = Retailer.query.all()
     page = int(request.args.get('page', 1))
     fields = [
         'id',
@@ -134,24 +136,20 @@ def orders():
             for msg in e.args:
                 flash(msg, 'error')
     retailer_id = int(request.args.get('retailer_id'))
-    tabapp.utils.execute(cur, '''
-        SELECT
-            product_id,
-            retailer.name as retailer_name,
-            array_agg(retailer_product.id) as product_order_ids
-        FROM retailer_product
-        JOIN retailer ON retailer.id = retailer_product.retailer_id
-        WHERE retailer_id = %s
-        AND sold_date IS NULL
-        GROUP BY 1, 2
-    ''', (retailer_id,))
-    rows = cur.fetchall()
-    product_orders = structure_from_rows(rows)
-    tabapp.utils.execute(cur, '''
-        SELECT id, name
-        FROM retailer
-    ''')
-    retailers = cur.fetchall()
+    retailer_products = db.session.query(
+        RetailerProduct.product_id,
+        Retailer.name,
+        sqlalchemy.func.array_agg(
+            RetailerProduct.id,
+            type_=sqlalchemy.dialects.postgresql.ARRAY(db.Integer)
+        ).label('product_order_ids')
+    ).filter(RetailerProduct.sold_date==None).join(Retailer).group_by(
+        RetailerProduct.product_id,
+        Retailer.name
+    )
+    current_app.logger.debug(str(retailer_products))
+    product_orders = structure_from_rows(retailer_products)
+    retailers = Retailer.query.all()
     context = {
         'retailer_id': retailer_id,
         'product_orders': product_orders,
@@ -165,30 +163,24 @@ def orders():
 def invoices():
     if request.method == 'POST':
         pass
-        #try: return sold_product_order(request.form)
-        #except Exception as e:
-            #for msg in e.args:
-                #flash(msg, 'error')
+        try: return sold_product_order(request.form)
+        except Exception as e:
+            for msg in e.args:
+                flash(msg, 'error')
     retailer_id = int(request.args.get('retailer_id'))
-    tabapp.utils.execute(cur, '''
-        SELECT
-            product_id,
-            retailer.name as retailer_name,
-            array_agg(retailer_product.id) as product_order_ids
-        FROM retailer_product
-        JOIN retailer ON retailer.id = retailer_product.retailer_id
-        WHERE retailer_id = %s
-        AND sold_date IS NOT NULL
-        AND payment_date IS NULL
-        GROUP BY 1, 2
-    ''', (retailer_id,))
-    rows = cur.fetchall()
-    product_orders = structure_from_rows(rows)
-    tabapp.utils.execute(cur, '''
-        SELECT id, name
-        FROM retailer
-    ''')
-    retailers = cur.fetchall()
+    retailer_products = db.session.query(
+        RetailerProduct.product_id,
+        Retailer.name,
+        sqlalchemy.func.array_agg(
+            RetailerProduct.id,
+            type_=sqlalchemy.dialects.postgresql.ARRAY(db.Integer)
+        ).label('product_order_ids')
+    ).filter(RetailerProduct.payment_date==None).join(Retailer).group_by(
+        RetailerProduct.product_id,
+        Retailer.name
+    )
+    product_orders = structure_from_rows(retailer_products)
+    retailers = Retailer.query.all()
     context = {
         'retailer_id': retailer_id,
         'product_orders': product_orders,

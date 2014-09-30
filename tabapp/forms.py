cc9c02081621b16150a119a0900b33a77b3f3d46
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-09-30 23:25:45 +0200

    Update retailer form

--- tabapp/forms.py
+++ tabapp/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
-from wtforms import FormField, TextField, FieldList, DecimalField
+from wtforms import TextField
+from wtforms.fields.html5 import DecimalField
 from wtforms.validators import DataRequired, NumberRange
 
 
@@ -7,7 +8,7 @@ class RetailerForm(Form):
     name = TextField('Name', validators=[DataRequired()])
     fees_proportion = DecimalField('Fees', validators=[DataRequired(), NumberRange(0, 100)])
     address = TextField('Address')
-    zip = TextField('Zip', validators=[DataRequired()])
+    zip = TextField('Zip')
     city = TextField('City')
     country = TextField('Country')
     contact_firstname = TextField('Contact firstname', validators=[DataRequired()])

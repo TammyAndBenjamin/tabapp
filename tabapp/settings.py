e1f3130b3fe4b8e715e54c48a2368ef3720c1695
Jonathan Camile <jonathan.camile+github@gmail.com>
2014-10-31 11:45:33 +0000

    Remove not default values from settings

--- tabapp/settings.py
+++ tabapp/settings.py
@@ -1,8 +1,5 @@
 import decimal
 
-SERVER_NAME = 'dev.tammyandbenjamin.com:80'
-SECRET_KEY = '8Xcb0dBV3ep`]=TC+LnY'
-
 SQLALCHEMY_DATABASE_URI = 'postgresql://tammyandbenjamin@localhost/tammyandbenjamin'
 
 APP_VAT = decimal.Decimal(1.2)

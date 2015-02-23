16326fff5d4e2e38ae6946b70c149df401a45cae
Jonathan Camile <jonathan.camile+github@gmail.com>
2015-02-23 22:17:27 +0000

    DB: Remove LimitingQuery for autofilter on enabled

--- tabapp/models/__init__.py
+++ tabapp/models/__init__.py
@@ -1,35 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from flask.ext.sqlalchemy import SQLAlchemy, Model, BaseQuery
+from flask.ext.sqlalchemy import SQLAlchemy, BaseQuery
 
-
-class LimitingQuery(BaseQuery):
-    def get(self, ident):
-        # override get() so that the flag is always checked in the 
-        # DB as opposed to pulling from the identity map. - this is optional.
-        return BaseQuery.get(self.populate_existing(), ident)
-
-    def __iter__(self):
-        return BaseQuery.__iter__(self.private())
-
-    def from_self(self, *ent):
-        # override from_self() to automatically apply
-        # the criterion too. This works with count() and
-        # others.
-        return BaseQuery.from_self(self.private(), *ent)
-
-    def private(self):
-        mzero = self._mapper_zero()
-        if mzero is not None:
-            crit = mzero.class_.enabled is True
-            return self.enable_assertions(False).filter(crit)
-        else:
-            return self
-
-
-#Model.query_class = LimitingQuery
-db = SQLAlchemy(session_options={'query_cls': LimitingQuery})
-
+db = SQLAlchemy(session_options={'expire_on_commit': False})
 
 from tabapp.models.contact import Contact
 from tabapp.models.product_cost import ProductCost
